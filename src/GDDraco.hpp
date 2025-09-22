1a00bec27e3f5cd5f14a92abb380812953981595
Lebi <lebidarra@gmail.com>
2025-09-22 12:21:45 +0100

    Working on basic Draco Extension

---
+++ src/GDDraco.hpp
@@ -0,0 +1,23 @@
+#ifndef GD_DRACO_HPP
+#define GD_DRACO_HPP
+
+#include <godot_cpp/classes/gltf_document_extension.hpp>
+#include <godot_cpp/classes/gltf_mesh.hpp>
+
+namespace godot {
+    class GDDraco: public GLTFDocumentExtension {
+        GDCLASS(GDDraco,GLTFDocumentExtension);
+
+        protected:
+            static void _bind_methods();
+
+        public:
+            GDDraco();
+            ~GDDraco();
+    
+            // Override this method to handle mesh compression
+            Error _parse_node_extensions(const Ref<GLTFState> &p_state, const Ref<GLTFNode> &p_gltf_node, const Dictionary &p_extensions) override;
+    };
+}
+
+#endif //GD_DRACO_HPP
